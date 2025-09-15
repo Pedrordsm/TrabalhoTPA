622401558d23a9532430b9eae4318cbbfc128090
TertS <tertuliano.s.j@gmail.com>
2025-09-15 09:54:25 -0300

    grafico

--- src/main/java/org/example/Menu.java
+++ src/main/java/org/example/Menu.java
@@ -3,7 +3,7 @@ package org.example;
 public class Menu {
     public void mostrarMenu(){
         System.out.println("Digite uma das opcoes abaixo\n" +
-                            "0 - Sair"              +
+                            "0 - Sair\n"              +
                              "1 - Inserir elemento\n"                    +
                                 "2 - Deletar elemento\n"                 +
                                     "3 - Buscar elemento\n"              +
