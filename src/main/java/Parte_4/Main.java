63a36e6806edf8b2285ea649077f0a06ae0400d3
Pedro Renã <pedrorenanmoreira@gmail.com>
2025-09-14 22:55:14 -0300

    Alterando prints para nanosegundos e prints da lista vetorial

--- src/main/java/Parte_4/Main.java
+++ src/main/java/Parte_4/Main.java
@@ -13,21 +13,21 @@ public class Main {
         Long tempoInicio, tempoFim, duracao;
 
 
-        tempoInicio = System.currentTimeMillis();
+        tempoInicio = System.nanoTime();
         System.out.println("Criando Lista Encadeada!");
         arq.gravandoList(listaLk);
 
-        tempoFim = System.currentTimeMillis();
+        tempoFim = System.nanoTime();
         duracao = tempoFim-tempoInicio;
-        System.out.println("Tempo decorrido da execução: " + duracao + " milisegundos");
+        System.out.println("Tempo decorrido da execução: " + duracao + " nanosegundos");
 
-        tempoInicio = System.currentTimeMillis();
+        tempoInicio = System.nanoTime();
         System.out.println("Criando Lista Vetorial!");
         arq.gravandoList(listaAR);
 
-        tempoFim = System.currentTimeMillis();
+        tempoFim = System.nanoTime();
         duracao = tempoFim-tempoInicio;
-        System.out.println("Tempo decorrido da execução: " + duracao + " milisegundos");
+        System.out.println("Tempo decorrido da execução: " + duracao + " nanosegundos");
 
 
         String menu = "Selecione uma modalidade:\n" +
@@ -51,109 +51,109 @@ public class Main {
                 if (opcao == 1) {
                     System.out.println("Inserir elemento ao início das listas!");
 
-                    tempoInicio = System.currentTimeMillis();
+                    tempoInicio = System.nanoTime();
                     System.out.println("Inserindo elemento no início da Lista Encadeada!");
                     listaLk.addFirst(new Aluno(111333, "Individou Teste", 10));
-                    tempoFim = System.currentTimeMillis();
+                    tempoFim = System.nanoTime();
                     duracao = tempoFim-tempoInicio;
-                    System.out.println("Tempo decorrido da execução: " + duracao + " milisegundos");
+                    System.out.println("Tempo decorrido da execução: " + duracao + " nanosegundos");
 
-                    tempoInicio = System.currentTimeMillis();
-                    System.out.println("Inserindo elemento no início da Lista Encadeada!");
+                    tempoInicio = System.nanoTime();
+                    System.out.println("Inserindo elemento no início da Lista Vetorial!");
                     listaAR.addFirst(new Aluno(111333, "Individou Teste", 10));
-                    tempoFim = System.currentTimeMillis();
+                    tempoFim = System.nanoTime();
                     duracao = tempoFim-tempoInicio;
-                    System.out.println("Tempo decorrido da execução: " + duracao + " milisegundos");
+                    System.out.println("Tempo decorrido da execução: " + duracao + " nanosegundos");
 
                 } else if (opcao == 2) {
 
                     System.out.println("Inserir elemento ao fim das listas!");
 
-                    tempoInicio = System.currentTimeMillis();
+                    tempoInicio = System.nanoTime();
                     System.out.println("Inserindo elemento no fim da Lista Encadeada!");
                     listaLk.addLast(new Aluno(111333, "Individou Teste", 10));
-                    tempoFim = System.currentTimeMillis();
+                    tempoFim = System.nanoTime();
                     duracao = tempoFim-tempoInicio;
-                    System.out.println("Tempo decorrido da execução: " + duracao + " milisegundos");
+                    System.out.println("Tempo decorrido da execução: " + duracao + " nanosegundos");
 
-                    tempoInicio = System.currentTimeMillis();
-                    System.out.println("Inserindo elemento no fim da Lista Encadeada!");
+                    tempoInicio = System.nanoTime();
+                    System.out.println("Inserindo elemento no fim da Lista Vetorial!");
                     listaAR.addLast(new Aluno(111333, "Individou Teste", 10));
-                    tempoFim = System.currentTimeMillis();
+                    tempoFim = System.nanoTime();
                     duracao = tempoFim-tempoInicio;
-                    System.out.println("Tempo decorrido da execução: " + duracao + " milisegundos");
+                    System.out.println("Tempo decorrido da execução: " + duracao + " nanosegundos");
 
                 }else if (opcao == 3) {
 
                     System.out.println("Inserir elemento no meio das listas!");
 
-                    tempoInicio = System.currentTimeMillis();
+                    tempoInicio = System.nanoTime();
                     System.out.println("Inserindo elemento no meio da Lista Encadeada!");
                     listaLk.add((listaLk.size())/2,new Aluno(111333, "Individou Teste", 10));
-                    tempoFim = System.currentTimeMillis();
+                    tempoFim = System.nanoTime();
                     duracao = tempoFim-tempoInicio;
-                    System.out.println("Tempo decorrido da execução: " + duracao + " milisegundos");
+                    System.out.println("Tempo decorrido da execução: " + duracao + " nanosegundos");
 
-                    tempoInicio = System.currentTimeMillis();
-                    System.out.println("Inserindo elemento no meio da Lista Encadeada!");
+                    tempoInicio = System.nanoTime();
+                    System.out.println("Inserindo elemento no meio da Lista Vetorial!");
                     listaAR.add((listaAR.size())/2,new Aluno(111333, "Individou Teste", 10));
-                    tempoFim = System.currentTimeMillis();
+                    tempoFim = System.nanoTime();
                     duracao = tempoFim-tempoInicio;
-                    System.out.println("Tempo decorrido da execução: " + duracao + " milisegundos");
+                    System.out.println("Tempo decorrido da execução: " + duracao + " nanosegundos");
 
                 }else if (opcao == 4) {
 
                     System.out.println("Buscar ultimo elemento das listas!");
 
-                    tempoInicio = System.currentTimeMillis();
+                    tempoInicio = System.nanoTime();
                     System.out.println("Buscando o ultimo elemento da Lista Encadeada!");
                     System.out.println(listaLk.get(listaLk.size()-1));
-                    tempoFim = System.currentTimeMillis();
+                    tempoFim = System.nanoTime();
                     duracao = tempoFim-tempoInicio;
-                    System.out.println("Tempo decorrido da execução: " + duracao + " milisegundos");
+                    System.out.println("Tempo decorrido da execução: " + duracao + " nanosegundos");
 
-                    tempoInicio = System.currentTimeMillis();
-                    System.out.println("Buscando o ultimo elemento da Lista Encadeada!");
-                    System.out.println(listaAR.get(listaLk.size()-1));
-                    tempoFim = System.currentTimeMillis();
+                    tempoInicio = System.nanoTime();
+                    System.out.println("Buscando o ultimo elemento da Lista Vetorial!");
+                    System.out.println(listaAR.get(listaAR.size()-1));
+                    tempoFim = System.nanoTime();
                     duracao = tempoFim-tempoInicio;
-                    System.out.println("Tempo decorrido da execução: " + duracao + " milisegundos");
+                    System.out.println("Tempo decorrido da execução: " + duracao + " nanosegundos");
 
                 }else if (opcao == 5) {
 
                     System.out.println("Buscar penultimo elemento das listas!");
 
-                    tempoInicio = System.currentTimeMillis();
+                    tempoInicio = System.nanoTime();
                     System.out.println("Buscando o penultimo elemento da Lista Encadeada!");
                     System.out.println(listaLk.get(listaLk.size()-2));
-                    tempoFim = System.currentTimeMillis();
+                    tempoFim = System.nanoTime();
                     duracao = tempoFim-tempoInicio;
-                    System.out.println("Tempo decorrido da execução: " + duracao + " milisegundos");
+                    System.out.println("Tempo decorrido da execução: " + duracao + " nanosegundos");
 
-                    tempoInicio = System.currentTimeMillis();
+                    tempoInicio = System.nanoTime();
                     System.out.println("Buscando o penultimo elemento da Lista Vetorial!");
-                    System.out.println(listaAR.get(listaLk.size()-2));
-                    tempoFim = System.currentTimeMillis();
+                    System.out.println(listaAR.get(listaAR.size()-2));
+                    tempoFim = System.nanoTime();
                     duracao = tempoFim-tempoInicio;
-                    System.out.println("Tempo decorrido da execução: " + duracao + " milisegundos");
+                    System.out.println("Tempo decorrido da execução: " + duracao + " nanosegundos");
 
                 }else if (opcao == 6) {
 
                     System.out.println("Buscar elemento no meio das listas!");
 
-                    tempoInicio = System.currentTimeMillis();
+                    tempoInicio = System.nanoTime();
                     System.out.println("Buscando o elemento no meio da Lista Encadeada!");
                     System.out.println(listaLk.get(listaLk.size()/2));
-                    tempoFim = System.currentTimeMillis();
+                    tempoFim = System.nanoTime();
                     duracao = tempoFim-tempoInicio;
-                    System.out.println("Tempo decorrido da execução: " + duracao + " milisegundos");
+                    System.out.println("Tempo decorrido da execução: " + duracao + " nanosegundos");
 
-                    tempoInicio = System.currentTimeMillis();
+                    tempoInicio = System.nanoTime();
                     System.out.println("Buscando o elemento no meio da Lista Vetorial!");
-                    System.out.println(listaAR.get(listaLk.size()/2));
-                    tempoFim = System.currentTimeMillis();
+                    System.out.println(listaAR.get(listaAR.size()/2));
+                    tempoFim = System.nanoTime();
                     duracao = tempoFim-tempoInicio;
-                    System.out.println("Tempo decorrido da execução: " + duracao + " milisegundos");
+                    System.out.println("Tempo decorrido da execução: " + duracao + " nanosegundos");
 
                 }
 
@@ -164,7 +164,7 @@ public class Main {
         }while (opcao != 0);
 
 
-        System.out.println(listaLk);
+        //System.out.println(listaLk);
         //System.out.println(listaAR);
 
 }
